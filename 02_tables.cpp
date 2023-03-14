3f1f5cce96cc864d98cc88af5fa6f4dfa5e9b2a2
Varun Majumdar <116209310+krosmos@users.noreply.github.com>
2023-03-14 18:10:56 +0530

    Add files via upload

---
+++ 02_tables.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class tables{
+    int n,product;
+    public:
+    void getval();
+    int diplaytab(){
+        int i=1;
+        while(i<=10){
+            cout<<n<<"x"<<i<<"="<<n*i<<endl;
+            i++;
+        }
+    } 
+};
+void tables :: getval(){
+    cout<<"Enter no. to display table: ";
+    cin>>n;
+}
+int main(){
+    tables a;
+    a.getval();
+    a.diplaytab();
+    return 0;
+}
