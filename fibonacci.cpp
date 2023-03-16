4eb10edb5618d3c25777e3abbedcd529bee8b10c
Varun Majumdar <116209310+krosmos@users.noreply.github.com>
2023-03-16 20:23:12 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class fibo{
+    int i,n,t1,t2,NT;
+    public:
+    void getval(){
+        cout<<"enter number of terms: ";
+        cin>>n;
+        t1=0, t2=1, NT=t1+t2;
+    }
+    void display(){
+        getval();
+        cout<<t1<<","<<t2;
+    for(i=3;i<=n;++i){
+        cout<<","<<NT;
+        t1=t2;
+        t2=NT;
+        NT=t1+t2;
+    }
+    }
+};
+int main(){
+    fibo s;
+    s.display();
+    return 0;
+}
